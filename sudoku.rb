9ded04141b366519b74a88521966274e23dcb700
zack <omkszyk@gmail.com>
2019-04-13 18:13:09 +0900

    仮問題とファイルの入力、かんたんな表示を実装

---
+++ sudoku.rb
@@ -0,0 +1,47 @@
+class Sudoku
+
+    def initialize(filename)
+        f = File.open(filename, mode = "rt")
+        sudoku = []
+        9.times do
+
+            line = []
+
+            3.times do
+
+                vector = []
+
+                3.times do
+                    vector.push(f.getc.to_i)
+                end
+
+                line.push(vector)
+            end
+
+            sudoku.push(line)
+            f.getc #改行送り
+        end
+
+        @sudoku = sudoku
+        f.close
+    end
+
+    def show
+        @sudoku.each do |line|
+            line.each do |vector|
+                vector.each do |num|
+                    if num == 0
+                        print " "
+                    else
+                        print num
+                    end
+                end
+            end
+            puts ""
+        end
+
+    end
+end
+
+sudoku = Sudoku.new(ARGV[0])
+sudoku.show
