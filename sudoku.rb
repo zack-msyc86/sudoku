c27a9837c94ab3aec832e07883d00e9539954566
Unknown <omkszyk@gmail.com>
2019-07-11 05:01:42 +0900

    solver tuketa

--- sudoku.rb
+++ sudoku.rb
@@ -49,6 +49,7 @@ class Sudoku < Array
     end
 =begin
     def transpose #転置
+        sudoku3 = []
         10.times do |i|
             sudoku = []
             sudoku2 = []
@@ -62,23 +63,96 @@ class Sudoku < Array
                 sudoku2.push(line2)
             end
             sudoku2.transpose.each do |line|
-                sudoku[i].push(line.each_slice(3).to_a)
+                sudoku.push(line.each_slice(3).to_a)
             end
+            sudoku3[i] =
         end
         @sudoku = sudoku
     end
 =end
-    def check
 
+    def hSolve
+        9.times do |num|
+            sudoku = @sudoku[num+1]
+            9.times do |i|
+                #puts sudoku[i].flatten.count(0)
+                if sudoku[i].flatten.count(0) == 1
+                    3.times do |j|
+                        3.times do |k|
+                            if sudoku[i][j][k] == 0
+                                @sudoku[0][i][j][k] = sudoku[i][j][k] = num+1
+                                printf("Horisontaly solve!:(%d,%d)->%d\n",i+1,j*3+k+1,num+1)
+                            end
+                        end
+                    end
+                end
+            end
+            @sudoku[num+1] = sudoku
+        end
+    end
+
+    def vSolve
+        9.times do |num|
+            sudoku = @sudoku[num+1]
+            3.times do |i|
+                3.times do |j|
+                    marker = []
+                    9.times do |k|
+                        if sudoku[k][i][j] == 0
+                            marker.push([k,i,j])
+                        end
+                    end
+                    #p marker.count
+                    if marker.count == 1
+                        #p marker
+                        @sudoku[0][marker[0][0]][marker[0][1]][marker[0][2]] = sudoku[marker[0][0]][marker[0][1]][marker[0][2]] = num+1
+                        printf("Verticaly solve!:(%d,%d)->%d\n",marker[0][0]+1,marker[0][1]*3+marker[0][2]+1,num+1)
+                    end
+                end
+            end
+            @sudoku[num+1] = sudoku
+        end
+    end
+
+    def bSolve
+        9.times do |num|
+            sudoku = @sudoku[num+1]
+            3.times do |i|
+                3.times do |j|
+                    marker = []
+                    3.times do |k|
+                        3.times  do |l|
+                            if sudoku[i*3+k][j][l] == 0
+                                marker.push([i*3+k,j,l])
+                            end
+                        end
+                    end
+                    if marker.count == 1
+                        @sudoku[0][marker[0][0]][marker[0][1]][marker[0][2]] = sudoku[marker[0][0]][marker[0][1]][marker[0][2]] = num+1
+                        printf("Box solve!:(%d,%d)->%d\n",marker[0][0]+1,marker[0][1]*3+marker[0][2]+1,num+1)
+                    end
+                end
+            end
+            @sudoku[num+1] = sudoku
+        end
     end
 
     def fill
-        1.times do |num|
-            puts num+1
+        9.times do |num|
+            #puts num+1
             hFlag = Array.new(9, false)
             vFlag = Array.new(9, false)
             bFlag = Array.new(9, false)
+
             sudoku = []
+            9.times do |i|
+                line = []
+                3.times do |j|
+                    line[j] = [0,0,0]
+                end
+                sudoku[i] = line
+            end
+
 
 =begin            @sudoku[num+1].each do |line|
                 line.each do |vector|
@@ -94,39 +168,70 @@ class Sudoku < Array
                         if @sudoku[num+1][line][vector][value] == num+1
                             hFlag[line] = true
                             vFlag[vector*3 + value] = true
-                            bFlag[line*3 + vector] = true
+                            bFlag[line/3*3 + vector] = true
+                            sudoku[line][vector][value] = num+1
+                        end
+
+                        if @sudoku[0][line][vector][value] != 0 && @sudoku[0][line][vector][value] != num+1
+                            sudoku[line][vector][value] = "+"
+                        end
+                    end
+                end
+            end
+
+
+            9.times do |i|
+                if hFlag[i]
+                    3.times do |j|
+                        3.times do |k|
+                            if sudoku[i][j][k] == 0
+                                sudoku[i][j][k] = "+"
+                            end
                         end
                     end
                 end
             end
 
             9.times do |i|
-                if hFlag[i]
-                    line = Array.new(3, [num+1,num+1,num+1])
-                else
-                    line = Array.new(3, [0,0,0])
-                end
-                sudoku.push(line)
-            end
-
-            9.times do |i|
                 if vFlag[i]
-                    1.times do |j|
-                        printf("%d : %d,%d \n",i,i/3,i%3)
-                        sudoku[j][i/3][i%3] = num+1
+                    9.times do |j|
+                        #printf("%d -> 1 (%d, %d)\n",sudoku[j][i/3][i%3],j,i/3+i%3)
+                        #p sudoku[j]
+                        if sudoku[j][i/3][i%3] == 0
+                            sudoku[j][i/3][i%3] = "+"
+                        end
+                        #p sudoku[j]
+                        #p "----------"
                     end
                 end
             end
 
+            #p bFlag
+
+            9.times do |i|
+                if bFlag[i]
+                    3.times do |j|
+                        #p sudoku[i/3+j][i%3]
+                        3.times do |k|
+                            if sudoku[i/3*3+j][i%3][k] == 0
+                                sudoku[i/3*3+j][i%3][k] = "+"
+                            end
+                        end
+                        #printf("%d,%d\n",i/3+j,i%
+                    end
+                        #p sudoku[i/3+j][i%3].object_id
+                end
+            end
             @sudoku[num+1] = sudoku
         end
     end
 
+
 end
 
 def showall(sudoku)
-    1.times do |i|
-        sudoku.show(i+1)
+    10.times do |i|
+        sudoku.show(i)
     end
 end
 
@@ -134,4 +239,18 @@ sudoku = Sudoku.new(ARGV[0])
 showall(sudoku)
 sudoku.fill
 showall(sudoku)
-#sudoku[0].show
+puts "////////////////////"
+ARGV[1].to_i.times do
+    sudoku.hSolve
+    sudoku.fill
+    sudoku.vSolve
+    sudoku.fill
+    sudoku.bSolve
+    sudoku.fill
+
+    puts ""
+    showall(sudoku)
+    #sudoku.show(3)
+
+    puts "============================"
+end
