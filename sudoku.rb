209c343cdf94a2a3ec10271db1225c85edfb412c
Unknown <omkszyk@gmail.com>
2019-08-01 22:29:31 +0900

    2

--- sudoku.rb
+++ sudoku.rb
@@ -1,3 +1,6 @@
+$maxLoop = ARGV[1].to_i
+$analogy = 0
+
 class Sudoku < Array
 
     def initialize(filename)
@@ -45,33 +48,14 @@ class Sudoku < Array
             end
             puts ""
         end
-        puts "------------------------------"
     end
-=begin
-    def transpose #転置
-        sudoku3 = []
-        10.times do |i|
-            sudoku = []
-            sudoku2 = []
-            @sudoku[i].each do |line|
-                line2 = []
-                line.each do |vector|
-                    vector.each do |num|
-                        line2.push(num)
-                    end
-                end
-                sudoku2.push(line2)
-            end
-            sudoku2.transpose.each do |line|
-                sudoku.push(line.each_slice(3).to_a)
-            end
-            sudoku3[i] =
-        end
-        @sudoku = sudoku
+
+    def result
+        @sudoku[0].flatten.join
     end
-=end
 
     def hSolve
+        flag = false
         9.times do |num|
             sudoku = @sudoku[num+1]
             9.times do |i|
@@ -81,17 +65,21 @@ class Sudoku < Array
                         3.times do |k|
                             if sudoku[i][j][k] == 0
                                 @sudoku[0][i][j][k] = sudoku[i][j][k] = num+1
+                                flag = true
                                 printf("Horisontaly solve!:(%d,%d)->%d\n",i+1,j*3+k+1,num+1)
                             end
                         end
                     end
                 end
             end
-            @sudoku[num+1] = sudoku
+            self[num+1] = sudoku
+            self.fill
         end
+        return flag
     end
 
     def vSolve
+        flag = false
         9.times do |num|
             sudoku = @sudoku[num+1]
             3.times do |i|
@@ -106,15 +94,19 @@ class Sudoku < Array
                     if marker.count == 1
                         #p marker
                         @sudoku[0][marker[0][0]][marker[0][1]][marker[0][2]] = sudoku[marker[0][0]][marker[0][1]][marker[0][2]] = num+1
+                        flag = true
                         printf("Verticaly solve!:(%d,%d)->%d\n",marker[0][0]+1,marker[0][1]*3+marker[0][2]+1,num+1)
                     end
                 end
             end
-            @sudoku[num+1] = sudoku
+            self[num+1] = sudoku
+            self.fill
         end
+        return flag
     end
 
     def bSolve
+        flag = false
         9.times do |num|
             sudoku = @sudoku[num+1]
             3.times do |i|
@@ -129,12 +121,15 @@ class Sudoku < Array
                     end
                     if marker.count == 1
                         @sudoku[0][marker[0][0]][marker[0][1]][marker[0][2]] = sudoku[marker[0][0]][marker[0][1]][marker[0][2]] = num+1
+                        flag = true
                         printf("Box solve!:(%d,%d)->%d\n",marker[0][0]+1,marker[0][1]*3+marker[0][2]+1,num+1)
                     end
                 end
             end
-            @sudoku[num+1] = sudoku
+            self[num+1] = sudoku
+            self.fill
         end
+        return flag
     end
 
     def fill
@@ -153,15 +148,6 @@ class Sudoku < Array
                 sudoku[i] = line
             end
 
-
-=begin            @sudoku[num+1].each do |line|
-                line.each do |vector|
-                    if vector.include?(num+1)
-                        hFlag[num] = true
-                    end
-                end
-            end
-=end
             9.times do |line|
                 3.times do |vector|
                     3.times do |value|
@@ -219,38 +205,135 @@ class Sudoku < Array
                         end
                         #printf("%d,%d\n",i/3+j,i%
                     end
-                        #p sudoku[i/3+j][i%3].object_id
+                    #p sudoku[i/3+j][i%3].object_id
                 end
             end
             @sudoku[num+1] = sudoku
         end
     end
 
+=begin
+    def sum
+        sFlag = false
+        all = (1..9).to_a
+        9.times do |line|
+            3.times do |vector|
+                3.times do |value|
+                    9.times do |num|
+                        flag = true
+                        num+1
+                        removedNum = all - [num+1]
+                        8.times do |i|
+                            if @sudoku[removedNum[i]][line][vector][value] != "+" || @sudoku[removedNum[i]][line][vector][value] != removedNum[i]
+                                flag = false
+                            end
+                        end
+                        if flag
+                            @sudoku[num+1][line][vector][value] = num+1
+                            @sudoku[0][line][vector][value] = num+1
+                            sFlag = true
+                            printf("Sum solve!:(%d,%d)->%d\n",line+1,vector*3+value+1,num+1)
+                            self.fill
+                            self.show(num+1)
+                        end
+                    end
+                end
+            end
+        end
+        return sFlag
+    end
 
-end
+=begin
+    def analogy(analogyCount)
+        81.times do
+            num = $analogy/9
+            line = $analogy - $analogy/9*9
+            sudoku = @sudoku[num+1]
+            printf("$analogy=%d,num=%d,line=%d,count=%d\n",$analogy,num+1,line+1,analogyCount)
+            if sudoku[line].flatten.count(0) == 2
+                puts "count=2"
+                index = sudoku[line].flatten.each_index.select{|i| sudoku[line].flatten[i] == 0}
+                @sudoku[0][line][index[analogyCount]/3][index[analogyCount]-index[analogyCount]/3] = num+1
+                @sudoku[num+1][line][index[analogyCount]/3][index[analogyCount]-index[analogyCount]/3] = num+1
+                self.fill
+                printf("\nanalogy:(%d,%d)->%d\n\n",line+1,index[analogyCount]+1,num+1)
+                if analogyCount == 1
+                    $analogy += 1
+                end
+                return
+            elsif $analogy == 80
+                puts "analogy over"
+                exit
+            else
+                $analogy += 1
+            end
+        end
+    end
+=end
 
-def showall(sudoku)
-    10.times do |i|
-        sudoku.show(i)
+    def fin?
+        return !@sudoku[0].flatten.include?(0)
+    end
+
+    def copy
+        Marshal.load(Marshal.dump(self))
     end
 end
 
-sudoku = Sudoku.new(ARGV[0])
-showall(sudoku)
-sudoku.fill
-showall(sudoku)
-puts "////////////////////"
-ARGV[1].to_i.times do
-    sudoku.hSolve
-    sudoku.fill
-    sudoku.vSolve
-    sudoku.fill
-    sudoku.bSolve
-    sudoku.fill
-
-    puts ""
-    showall(sudoku)
-    #sudoku.show(3)
-
-    puts "============================"
+def showall(sudoku)
+    puts "---------"
+    10.times do |i|
+        sudoku.show(i)
+        puts "---------"
+    end
 end
+
+def solve(sudoku)
+    ARGV[1].to_i.times do |loop|
+
+        flag = false
+
+        hFlag = sudoku.hSolve
+        vFlag = sudoku.vSolve
+        bFlag = sudoku.bSolve
+        #sFlag = sudoku.sum
+        flag = hFlag || vFlag || bFlag
+        puts "\n"
+        if !flag
+            return
+        end
+
+        sudoku.show(0)
+        if ARGV[2].to_i != 0
+            puts "---------"
+            sudoku.show(ARGV[2].to_i)
+        end
+        printf("\nLOOP%d=============================\n\n",loop+1)
+    end
+end
+
+def main
+
+    if ARGV[1].to_i == 0
+        ARGV[1] = "100"
+    end
+
+    sudoku = Sudoku.new(ARGV[0])
+    sudoku.fill
+    showall(sudoku)
+    printf("\nDisplay Type:%d\nMax Loop:%d\n\nFin init======================\n\n",ARGV[2].to_i,ARGV[1].to_i)
+
+    ARGV[1].to_i.times do
+
+        solve(sudoku)
+
+        if sudoku.fin?
+            printf("\nfin!\n%s\n\n",sudoku.result)
+            sudoku.show(0)
+            return
+        end
+    end
+
+end
+
+main
